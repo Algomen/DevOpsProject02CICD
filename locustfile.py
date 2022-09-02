3c41aea3c3ec537e60a9fc360a6f728b7fe361f8
Álvaro Gómez <alvarogomen93@gmail.com>
2022-09-02 14:59:21 +0200

    uncomment post in locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -7,7 +7,7 @@ class WebsiteUser(HttpUser):
     @task
     def index(self):
         self.client.get("/")
-"""
+
     @task
     def predict(self):
         self.client.post("/predict", json={
@@ -51,4 +51,3 @@ class WebsiteUser(HttpUser):
               "0":29.93
            }
         })
-"""
